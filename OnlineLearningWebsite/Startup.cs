baa87ab64e13b6b2410f0349cca55b4f21a06bb4
seetarampatel <200393396@student.georgianc.on.ca>
2019-03-15 19:45:38 -0400

    Add project files.

---
+++ OnlineLearningWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnlineLearningWebsite.Startup))]
+namespace OnlineLearningWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
